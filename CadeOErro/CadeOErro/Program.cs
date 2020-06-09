c599f82f7ecaf21e39e3087ead63d94a44001d2d
LucasCancio <lucas.cancio7@gmail.com>
2020-06-09 20:51:21 -0300

    [refatoração] Reestruturando sistema

--- CadeOErro/CadeOErro/Program.cs
+++ CadeOErro/CadeOErro/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace CadeOErro
+namespace CadeOErro.Server
 {
     public class Program
     {
